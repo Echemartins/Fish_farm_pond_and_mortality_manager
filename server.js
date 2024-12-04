355831529a7c848c38eacd7d41efcb81f276c152
Echemartins <echemartins47@gmail.com>
2024-12-04 19:42:36 +0100

    first updte

--- server.js
+++ server.js
@@ -1,80 +1,40 @@
-const express = require("express");
-const mongoose = require("mongoose");
-// const router = require("./routes/route");
-// const multer = require("multer");
-// const path = require("path");
+
+const mongoose = require("mongoose")
+const port = 5000
+const express = require("express")
+const app = express()
+const uri = 'mongodb+srv://echemartins47:password1234@cluster0.jlopo.mongodb.net/pond-manager?retryWrites=true&w=majority&appName=Cluster0'
+const DBURL='mongodb+srv://promise:promise123@real.jme6j.mongodb.net/lmtechub?retryWrites=true&w=majority'
 const ejs = require('ejs')
 const methodOveride = require('method-override')
 const pondController = require("./controllers/pondController");
 const Mortality = require("./models/mortalityModel");
-const mortalityController = require("./controllers/mortalityController")
-const app = express();
-// const session = require("express-session");
-// const { login } = require("./controllers/userController");
-// const isAuth = require("./middlewares/auth");
-// const Post = require("./models/postmodel");
-
-const port = 4000;
+const mortalityController = require("./controllers/mortalityController");
 app.use(express.json());
 app.use(methodOveride('_method'))
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(
-//     session({
-//         secret: "echemartins", // Change this to a secret key for session encryption (keep it secure)
-//         resave: false,
-//         saveUninitialized: false,
-//     })
-// );
-
 app.get("/",pondController.getAllPonds)
 app.get("/createpond",(req,res)=>{
     res.render("createpond")
 })
-// app.get("/viewpond",pondController.getPondDetails)
 app.get('/ponds/search',pondController.searchPonds)
 app.get('/ponds/:pondId', pondController.getPondDetails);
 app.post("/ponds/:pondId/mortality",mortalityController.addMortality)
 app.delete("/ponds/:pondId",pondController.deletePostById)
-
-// app.use("/api", router);
 app.post("/createPond", pondController.createPond);
-// app.get("/contact", (req, res) => {
-//     res.render("contact");
-// });
-// app.get("/login", (req, res) => {
-//     res.render("login");
-// });
-// app.get("/signup", (req, res) => {
-//     res.render("signUp");
-// });
-// app.get('/members',(req,res)=>{
-//     res.render('members')
-// })
 
-mongoose
-    .connect(
-        "mongodb://127.0.0.1:27017/mortalityManager"
-        // {useNewurlParser:true,
-        //     useUnifiedTopology:true
-        // }
-    )
-    .then(() => {
-        console.log("connected to mongodb");
-    })
-    .catch((error) => {
-        console.error("mongoDb connection error", error);
-    });
 
-app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
-});
 
-// async function checkPost(){
-//   const posts = await Post.find({})
-//   console.log(posts[posts.length - 1])
-// }
 
-// checkPost()
+
+
+
+app.listen(port,()=>{
+console.log(`app is running in port ${port}`)})
+mongoose.connect(DBURL)
+.then(()=> console.log('connected to database'))
+.catch((err)=>{console.log('error connecting to database', err)}) 
+
